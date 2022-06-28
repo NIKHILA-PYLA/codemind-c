72b7967eaaf5b3ba09f471d9539326aaef39b7ea
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-06-28 23:02:31 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int hcf=1,i,max;     if (a>b)     {         max=a;     }     else     {         max=b;     }     for (i=1;i<max;i++)     {         if(a%i==0 && b%i==0)         {           if (i>hcf)           {              hcf=i;           }         }     }     printf("%d",hcf);     return 0; }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int hcf=1,i,max;
+    if (a>b)
+    {
+        max=a;
+    }
+    else
+    {
+        max=b;
+    }
+    for (i=1;i<max;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+          if (i>hcf)
+          {
+             hcf=i;
+          }
+        }  
+    }
+    printf("%d",hcf);
+    return 0;
+}

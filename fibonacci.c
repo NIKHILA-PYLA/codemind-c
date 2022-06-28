6510255a455f742c5bb6ef5587d4482fe7bdd2a4
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-06-28 23:20:41 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int fa=0,fb=1,temp,i;     for (i=0;i<a;i++)   {       printf("%d ",fa);       temp=fa+fb;       fa=fb;       fb=temp;   }   return 0; }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int fa=0,fb=1,temp,i;
+    for (i=0;i<a;i++)
+  {
+      printf("%d ",fa);
+      temp=fa+fb;
+      fa=fb;
+      fb=temp;
+  }
+  return 0;
+}
+    

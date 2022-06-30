98f49a21b016f5b56d24cf1c257a2db648fb8fc2
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-06-30 23:00:05 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     int se=0,so=0;     for (i=0; i<a; i++)     {         if (i%2==0)         {             se+=arr[i];         }         else         {             so+=arr[i];         }     }     int diff=se-so;     if (diff<0)     {         diff*=-1;     }     printf("%d",diff);     return 0; }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int se=0,so=0;
+    for (i=0; i<a; i++)
+    {
+        if (i%2==0)
+        {
+            se+=arr[i];
+        }
+        else
+        {
+            so+=arr[i];
+        }
+    }
+    int diff=se-so;
+    if (diff<0)
+    {
+        diff*=-1;
+    }
+    printf("%d",diff);
+    return 0;
+}
+

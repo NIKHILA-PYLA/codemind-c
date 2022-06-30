e15d4ea3e07c5fb0e16864b04c87aaab8cc8be8b
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-06-30 23:29:38 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     for (i=0; i<a; i++)     {         if (arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     }     for (i=0; i<a; i++)     {         if (arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     return 0; }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (i=0; i<a; i++)
+    {
+        if (arr[i]%2!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for (i=0; i<a; i++)
+    {
+        if (arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    return 0;
+}

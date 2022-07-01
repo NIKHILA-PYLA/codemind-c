829d08ed386917ce2151e7a2b461b6685f8504a7
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-07-01 23:47:26 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     char arr[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};     int i,j;     for (i=0; i<a; i++)     {         for (j=0; j<a; j++)         {             printf("%c ",arr[i]);             if (j==a-1)             {                 printf(" ");             }         }     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,23 +3,19 @@ int main()
 {
     int a;
     scanf("%d",&a);
+    char arr[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i,j;
     for (i=0; i<a; i++)
     {
-        for (j=0; j<a-(i+1); j++)
+        for (j=0; j<a; j++)
         {
-            printf(" ");
-        }
-        for (j=i; j>0; j--)
-        {
-            printf("%d",j);
-        }
-        for (j=0; j<i+1; j++)
-        {
-            printf("%d",j);
-        }
-        printf("
+            printf("%c ",arr[i]);
+            if (j==a-1)
+            {
+                printf("
 ");
+            }
+        }
     }
     return 0;
 }

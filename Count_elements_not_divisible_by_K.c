b2132ab224a68e8a7a29976d4a7efa9836b71aac
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-07-01 11:01:31 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int arr[a],i;     for (i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     int c=0;     for (i=0; i<a; i++)     {         if (arr[i]%b!=0)         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int arr[a],i;
+    for (i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0;
+    for (i=0; i<a; i++)
+    {
+        if (arr[i]%b!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
+
